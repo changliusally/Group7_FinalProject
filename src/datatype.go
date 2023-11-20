92589b611aa23ac0452ed0ed61d0c6228a3c50bf
YuningZ <jinglingmk@126.com>
2023-11-19 21:45:56 -0500

    datatype
    23/11/19

---
+++ src/datatype.go
@@ -0,0 +1,37 @@
+package main
+
+//Global symbols
+
+//File absolute paths for importing functions
+var SRC_PATH = "../src"
+
+// struct
+type OrderedPair struct {
+	x float64
+	y float64
+}
+
+type Individual struct {
+	id       int
+	age      int
+	sex      int //0: male 1:famale
+	position OrderedPair
+	genetics int //single loci, 0:aa 1:Aa 2:AA
+	gen      int //which generation
+}
+
+type Population struct {
+	individuals []Individual
+	mutateAge   int
+	birthRate   float64
+	deathRate   float64
+}
+
+type Grid struct {
+	position      OrderedPair
+	landscapeType int //0:land 1:water
+}
+
+type Landscape struct {
+	grid [][]Grid
+}
