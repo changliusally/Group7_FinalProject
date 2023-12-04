355c637483067a217bcae2eff44b9fea31687143
Zhengdoge <yuningz3@andrew.cmu.edu>
2023-12-03 23:07:35 -0500

    update function and main

--- src/function.go
+++ src/function.go
@@ -14,8 +14,7 @@ func Loadfile(filename string, header bool) [][]string {
 	// Open the file
 	file, err := os.Open(filename) //
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		panic("Error opening file:", err)
 	}
 	defer file.Close()
 
@@ -26,14 +25,15 @@ func Loadfile(filename string, header bool) [][]string {
 	if header == true {
 		headers, err := reader.Read()
 		if err != nil {
-			fmt.Println("Error reading header:", err)
+			panic("Error reading header:", err)
 		}
+		fmt.Println(headers)
 	}
 
 	// Read in all the records
 	records, err := reader.ReadAll()
 	if err != nil {
-		fmt.Println("Error reading all records:", err)
+		panic("Error reading all records:", err)
 	}
 
 	return records
@@ -55,13 +55,22 @@ func ReadInputParameters(parameters []string) (Population, Landscape, Model, int
 	population.individuals = individuals
 
 	// second column is the int number of Monte-Carlo run
-	mcRun := strconv.Atoi(parameters[1])
+	mcRun, err1 := strconv.Atoi(parameters[1])
+	if err1 <= 0 {
+		panic("Error: Monte-Carlo run number is non-positive")
+	}
 
 	// third column is the int number of the simulation looptime
-	looptime := strconv.Atoi(parameters[2])
+	looptime, err2 := strconv.Atoi(parameters[2])
+	if err2 <= 0 {
+		panic("Error: simulation looptime is non-positive")
+	}
 
 	// fourth column is the int number of output year
-	outputYear := strconv.Atoi(parameters[3])
+	outputYear, err3 := strconv.Atoi(parameters[3])
+	if err3 <= 0 {
+		panic("Error: output year is non-positive")
+	}
 
 	// fifth column is the filename of cdmatrix
 	cdmatrix := parameters[4]
@@ -72,16 +81,28 @@ func ReadInputParameters(parameters []string) (Population, Landscape, Model, int
 	cdmat := ReadCdmatrix(cdmatData)
 
 	// sixth column is the float number of mateFreq
-	population.mateFreq := strconv.ParseFloat(parameters[5], 64)
+	population.mateFreq, err4 := strconv.ParseFloat(parameters[5], 64)
+	if err4 < 0 {
+		panic("Error: mateFreq is negative")
+	}
 
 	// seventh column is the int number of matureAge
-	population.matureAge := strconv.Atoi(parameters[6])
+	population.matureAge, err5 := strconv.Atoi(parameters[6])
+	if err5 < 0 {
+		panic("Error: matureAge is negative")
+	}
 
 	// eighth column is the int number of fecundity
-	population.fecundity := strconv.Atoi(parameters[7])
+	population.fecundity, err6 := strconv.Atoi(parameters[7])
+	if err6 < 0 {
+		panic("Error: fecundity is negative")
+	}
 
 	// ninth column is the float number of femaleRate
-	population.femaleRate := strconv.ParseFloat(parameters[8], 64)
+	population.femaleRate, err7 := strconv.ParseFloat(parameters[8], 64)
+	if err7 < 0 {
+		panic("Error: femaleRate is negative")
+	}
 
 	// initialize the landscape
 	var landscape Landscape
@@ -91,28 +112,44 @@ func ReadInputParameters(parameters []string) (Population, Landscape, Model, int
 	model.popModel := parameters[9]
 
 	// eleventh column is the float number of density dependent growth rate	r	‘1.0’	The growth rate used in the density dependent functions above (‘logistic’, ‘richards’, and ‘rickers’).
-	model.r_env := strconv.ParseFloat(parameters[10], 64)
+	model.r_env, err8 := strconv.ParseFloat(parameters[10], 64)
+	if err9 < 0 {
+		panic("Error: density dependent growth rate is negative")
+	}
 
 	// twelfth column is the float number of environmental carrying capacity	K	. The carrying capacity used in the density dependent functions above (‘logistic’, ‘richards’, and ‘rickers’).
-	landscape.K_env := strconv.ParseFloat(parameters[11], 64)
+	landscape.K_env, err9 := strconv.ParseFloat(parameters[11], 64)
+	if err10 <= 0 {
+		panic("Error: environmental carrying capacity is non-positive")
+	}
 
 	// thirteenth column is the float number of carring capacity for every grid
-	landscape.K_grid := strconv.ParseFloat(parameters[12], 64)
+	landscape.K_grid, err10 := strconv.ParseFloat(parameters[12], 64)
+	if err11 < 0 {
+		panic("Error: carring capacity for every grid is negative")
+	}
 
 	// fourteenth column is the float number of the width of the landscape
-	landscape.width := strconv.Atoi(parameters[13])
+	landscape.width, err11 := strconv.Atoi(parameters[13])
+	if err12 < 0 {
+		panic("Error: width of the landscape is negative")
+	}
 
 	// fifteenth to seventeenth column is the float number of the fitness of genotype aa, Aa, AA
 	fitness := make([]float64, 3)
-	population.fitness[0] := strconv.ParseFloat(parameters[14], 64)
-	population.fitness[1] := strconv.ParseFloat(parameters[15], 64)
-	population.fitness[2] := strconv.ParseFloat(parameters[16], 64)
+	population.fitness[0], err12 := strconv.ParseFloat(parameters[14], 64)
+	population.fitness[1], err13 := strconv.ParseFloat(parameters[15], 64)
+	population.fitness[2], err14 := strconv.ParseFloat(parameters[16], 64)
+	if err12 < 0 || err13 < 0 || err14 < 0 {
+		panic("Error: fitness is negative")
+	}
 
 	return population, landscape, model, mcRun, looptime, outputYear, cdmat
 }
 
 // read xyfile
 // input: a slice of slice of string
+// output: a slice of individuals
 func ReadXyfile(individualData [][]string) individuals {
 	// initialize the individuals
 	var individuals []Individual
@@ -145,6 +182,7 @@ func ReadXyfile(individualData [][]string) individuals {
 
 // read cdmatrix
 // input: a slice of slice of string
+// output: a slice of slice of float64
 func ReadCdmatrix(records [][]string) [][]float64 {
 	// Convert string records to [][]float64
 	var matrix [][]float64
