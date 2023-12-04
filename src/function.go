8b33124182d098e19832dd90ee36857a9d1a6c3b
Zhengdoge <yuningz3@andrew.cmu.edu>
2023-12-03 23:27:48 -0500

    debug function

--- src/function.go
+++ src/function.go
@@ -14,7 +14,7 @@ func Loadfile(filename string, header bool) [][]string {
 	// Open the file
 	file, err := os.Open(filename) //
 	if err != nil {
-		panic("Error opening file:", err)
+		panic("Error opening file:")
 	}
 	defer file.Close()
 
@@ -25,7 +25,7 @@ func Loadfile(filename string, header bool) [][]string {
 	if header == true {
 		headers, err := reader.Read()
 		if err != nil {
-			panic("Error reading header:", err)
+			panic("Error reading header:")
 		}
 		fmt.Println(headers)
 	}
@@ -33,7 +33,7 @@ func Loadfile(filename string, header bool) [][]string {
 	// Read in all the records
 	records, err := reader.ReadAll()
 	if err != nil {
-		panic("Error reading all records:", err)
+		panic("Error reading all records:")
 	}
 
 	return records
@@ -56,19 +56,28 @@ func ReadInputParameters(parameters []string) (Population, Landscape, Model, int
 
 	// second column is the int number of Monte-Carlo run
 	mcRun, err1 := strconv.Atoi(parameters[1])
-	if err1 <= 0 {
+	if err1 != nil {
+		panic(err1)
+	}
+	if mcRun <= 0 {
 		panic("Error: Monte-Carlo run number is non-positive")
 	}
 
 	// third column is the int number of the simulation looptime
 	looptime, err2 := strconv.Atoi(parameters[2])
-	if err2 <= 0 {
+	if err2 != nil {
+		panic(err2)
+	}
+	if looptime <= 0 {
 		panic("Error: simulation looptime is non-positive")
 	}
 
 	// fourth column is the int number of output year
 	outputYear, err3 := strconv.Atoi(parameters[3])
-	if err3 <= 0 {
+	if err3 != nil {
+		panic(err3)
+	}
+	if outputYear <= 0 {
 		panic("Error: output year is non-positive")
 	}
 
@@ -81,68 +90,113 @@ func ReadInputParameters(parameters []string) (Population, Landscape, Model, int
 	cdmat := ReadCdmatrix(cdmatData)
 
 	// sixth column is the float number of mateFreq
-	population.mateFreq, err4 := strconv.ParseFloat(parameters[5], 64)
-	if err4 < 0 {
+	mateFreq, err4 := strconv.ParseFloat(parameters[5], 64)
+	if err4 != nil {
+		panic(err4)
+	}
+	if mateFreq < 0 {
 		panic("Error: mateFreq is negative")
 	}
+	population.mateFreq = mateFreq
 
 	// seventh column is the int number of matureAge
-	population.matureAge, err5 := strconv.Atoi(parameters[6])
-	if err5 < 0 {
+	matureAge, err5 := strconv.Atoi(parameters[6])
+	if err5 != nil {
+		panic(err5)
+	}
+	if matureAge < 0 {
 		panic("Error: matureAge is negative")
 	}
+	population.matureAge = matureAge
 
 	// eighth column is the int number of fecundity
-	population.fecundity, err6 := strconv.Atoi(parameters[7])
-	if err6 < 0 {
+	fecundity, err6 := strconv.Atoi(parameters[7])
+	if err6 != nil {
+		panic(err6)
+	}
+	if fecundity < 0 {
 		panic("Error: fecundity is negative")
 	}
+	population.fecundity = fecundity
 
 	// ninth column is the float number of femaleRate
-	population.femaleRate, err7 := strconv.ParseFloat(parameters[8], 64)
-	if err7 < 0 {
+	femaleRate, err7 := strconv.ParseFloat(parameters[8], 64)
+	if err7 != nil {
+		panic(err7)
+	}
+	if femaleRate < 0 {
 		panic("Error: femaleRate is negative")
 	}
+	population.femaleRate = femaleRate
 
 	// initialize the landscape
 	var landscape Landscape
 	var model Model
 
 	// tenth column is the string of population model
-	model.popModel := parameters[9]
+	model.popModel = parameters[9]
 
 	// eleventh column is the float number of density dependent growth rate	r	‘1.0’	The growth rate used in the density dependent functions above (‘logistic’, ‘richards’, and ‘rickers’).
-	model.r_env, err8 := strconv.ParseFloat(parameters[10], 64)
-	if err9 < 0 {
+	r_env, err8 := strconv.ParseFloat(parameters[10], 64)
+	if err8 != nil {
+		panic(err8)
+	}
+	if r_env < 0 {
 		panic("Error: density dependent growth rate is negative")
 	}
+	model.r_env = r_env
 
-	// twelfth column is the float number of environmental carrying capacity	K	. The carrying capacity used in the density dependent functions above (‘logistic’, ‘richards’, and ‘rickers’).
-	landscape.K_env, err9 := strconv.ParseFloat(parameters[11], 64)
-	if err10 <= 0 {
+	// twelfth column is the int number of environmental carrying capacity	K	. The carrying capacity used in the density dependent functions above (‘logistic’, ‘richards’, and ‘rickers’).
+	K_env, err9 := strconv.Atoi(parameters[11])
+	if err9 != nil {
+		panic(err9)
+	}
+	if K_env < 0 {
 		panic("Error: environmental carrying capacity is non-positive")
 	}
+	landscape.K_env = K_env
 
-	// thirteenth column is the float number of carring capacity for every grid
-	landscape.K_grid, err10 := strconv.ParseFloat(parameters[12], 64)
-	if err11 < 0 {
+	// thirteenth column is the int number of carring capacity for every grid
+	K_grid, err10 := strconv.Atoi(parameters[12])
+	if err10 != nil {
+		panic(err10)
+	}
+	if K_grid < 0 {
 		panic("Error: carring capacity for every grid is negative")
 	}
+	landscape.K_grid = K_grid
 
-	// fourteenth column is the float number of the width of the landscape
-	landscape.width, err11 := strconv.Atoi(parameters[13])
-	if err12 < 0 {
+	// fourteenth column is the int number of the width of the landscape
+	width, err11 := strconv.Atoi(parameters[13])
+	if err11 != nil {
+		panic(err11)
+	}
+	if width < 0 {
 		panic("Error: width of the landscape is negative")
 	}
+	landscape.width = width
 
 	// fifteenth to seventeenth column is the float number of the fitness of genotype aa, Aa, AA
 	fitness := make([]float64, 3)
-	population.fitness[0], err12 := strconv.ParseFloat(parameters[14], 64)
-	population.fitness[1], err13 := strconv.ParseFloat(parameters[15], 64)
-	population.fitness[2], err14 := strconv.ParseFloat(parameters[16], 64)
-	if err12 < 0 || err13 < 0 || err14 < 0 {
+	fitness_aa, err12 := strconv.ParseFloat(parameters[14], 64)
+	if err12 != nil {
+		panic(err12)
+	}
+	fitness_Aa, err13 := strconv.ParseFloat(parameters[15], 64)
+	if err13 != nil {
+		panic(err13)
+	}
+	fitness_AA, err14 := strconv.ParseFloat(parameters[16], 64)
+	if err14 != nil {
+		panic(err14)
+	}
+	fitness[0] = fitness_aa
+	fitness[1] = fitness_Aa
+	fitness[2] = fitness_AA
+	if fitness[0] < 0 || fitness[1] < 0 || fitness[2] < 0 {
 		panic("Error: fitness is negative")
 	}
+	population.fitness = fitness
 
 	return population, landscape, model, mcRun, looptime, outputYear, cdmat
 }
@@ -150,7 +204,7 @@ func ReadInputParameters(parameters []string) (Population, Landscape, Model, int
 // read xyfile
 // input: a slice of slice of string
 // output: a slice of individuals
-func ReadXyfile(individualData [][]string) individuals {
+func ReadXyfile(individualData [][]string) []Individual {
 	// initialize the individuals
 	var individuals []Individual
 
@@ -159,19 +213,49 @@ func ReadXyfile(individualData [][]string) individuals {
 		individuals[i] = Individual{}
 
 		// first column is the int number of X coordinate
-		individuals[i].position.x = strconv.Atoi(row[0])
+		individuals[i].position.x, err1 = strconv.Atoi(row[0])
+		if err1 != nil {
+			panic(err1)
+		}
+		if individuals[i].position.x < 0 {
+			panic("Error: X coordinate is negative")
+		}
 
 		// second column is the int number of Y coordinate
-		individuals[i].position.y = strconv.Atoi(row[1])
+		individuals[i].position.y, err2 = strconv.Atoi(row[1])
+		if err2 != nil {
+			panic(err2)
+		}
+		if individuals[i].position.y < 0 {
+			panic("Error: Y coordinate is negative")
+		}
 
 		// third column is the int number of the individual id
-		individuals[i].id = strconv.Atoi(row[2])
+		individuals[i].id, err3 = strconv.Atoi(row[2])
+		if err3 != nil {
+			panic(err3)
+		}
+		if individuals[i].id < 0 {
+			panic("Error: individual id is negative")
+		}
 
 		// fourth column is the int number of the individual age
-		individuals[i].age = strconv.Atoi(row[3])
+		individuals[i].age, err4 = strconv.Atoi(row[3])
+		if err4 != nil {
+			panic(err4)
+		}
+		if individuals[i].age < 0 {
+			panic("Error: individual age is negative")
+		}
 
 		// fifth column is the int number of individual sex
-		individuals[i].sex = strconv.Atoi(row[4])
+		individuals[i].sex, err5 = strconv.Atoi(row[4])
+		if err5 != nil {
+			panic(err5)
+		}
+		if individual[i].sex != 0 || individual.sex[i] != 1 {
+			panic("Error: sex wrong")
+		}
 
 		// sixth column is the string of individual genetics
 		individuals[i].genetics = row[5]
