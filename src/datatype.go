a5a17002ab547f2a423eb686b6689212ebcf19a2
yitianTracy <145805370+yitianTracy@users.noreply.github.com>
2023-12-04 20:56:38 -0500

    Update datatype.go

--- src/datatype.go
+++ src/datatype.go
@@ -51,3 +51,11 @@ type Model struct {
 	popModel string
 	r_env    float64
 }
+
+type Canvas struct {
+	gc     *draw2dimg.GraphicContext
+	img    image.Image
+	width  int // both width and height are in pixels
+	height int
+}
+
