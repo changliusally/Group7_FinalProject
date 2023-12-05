cf8d860b3b815876226bfa72e6127d0bb2ee7c38
yitianTracy <145805370+yitianTracy@users.noreply.github.com>
2023-12-04 20:56:19 -0500

    Update drawing.go

--- src/drawing.go
+++ src/drawing.go
@@ -7,14 +7,6 @@ import (
 	"math"
 )
 
-type Canvas struct {
-	gc     *draw2dimg.GraphicContext
-	img    image.Image
-	width  int // both width and height are in pixels
-	height int
-}
-
-
 // function DrawPopulation
 func DrawPopulation(population Population, landscape Landscape) Canvas {
 	canvasWidth := landscape.width
