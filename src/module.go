b1d0c5aba4903beef697662235a942195ec13818
YinuoYang327 <143552215+YinuoYang327@users.noreply.github.com>
2023-12-05 16:21:56 -0500

    Update module.go

--- src/module.go
+++ src/module.go
@@ -4,73 +4,71 @@ package main
 
 import (
 	"math/rand"
-	"time"
+	//"time"
+	"fmt"
 )
 
 //AdultDeath takes the population and totalDeathCount as input and returns the population
 //with certain inividuals deleted. The selection of death individuals is based on the
 //population's death rate, female rate, and genetics ratio
 func (population *Population) AdultDeath(totalDeathCount int) {
-	rand.Seed(time.Now().UnixNano())
-
-	// Calculate the total number of deaths based on the population size and death rate.
-	//number:=len(population.individuals)
-	//totalDeathCount := int(float64(number) * population.deathRate)
-
-	//death of female and male should meet population.femaleRate
-	femaleDeathCount := int(float64(totalDeathCount) * population.femaleRate)
-
-	// Calculate the number of males to die (total deaths minus female deaths).
-	maleDeathCount := totalDeathCount - femaleDeathCount
-
-	//Create a map to track the number of deaths for each genetic type (0, 1, 2) based on a 1:2:1 ratio.
-	geneticsCounts := map[int]int{
-		0: totalDeathCount / 4,     // Genotype aa
-		1: totalDeathCount / 2,     // Genotype Aa
-		2: totalDeathCount / 4,     // Genotype AA
-	}
-
-	// choose by random which individual to die and record the individual
-	// Slice to store the IDs of individuals who will die.
-	var deadIndividuals []int
-
-	// Continue selecting individuals for death until the required numbers are reached for each sex and genetic type.
-	for maleDeathCount > 0 || femaleDeathCount > 0 {
-		totalIndividual := len(population.individuals)
-		index := rand.Intn(totalIndividual)
-		individual := population.individuals[index]
-
-		// Skip if the individual is already selected for death.
-		//if contains(deadIndividuals, individual.id) || individual.age == 0 {
-		if contains(deadIndividuals, individual.id) {
-			continue
+	//check if the totalDeathCount is negative 
+	if totalDeathCount <=0 {
+		fmt.Println(totalDeathCount)
+	} else {
+		//check how many individuals are in the current population 
+		count := 0
+		individualToDie := make([]Individual, 0) //all death candidates 
+		for i:= range population.individuals {
+			if population.individuals[i].age > 0 {
+				individualToDie = append(individualToDie, population.individuals[i])
+				count++
+			}
 		}
 
-		if individual.sex == 1 && femaleDeathCount > 0 && geneticsCounts[individual.genetics] > 0 {
+		/*
+		femaleRate := population.femaleRate 
+		//death of female and male should meet population.femaleRate
+		femaleDeathCount := int(float64(totalDeathCount) * population.femaleRate)
+		// Calculate the number of males to die (total deaths minus female deaths).
+		maleDeathCount := totalDeathCount - femaleDeathCount
 
-			// Check and select female individuals for death.
-			femaleDeathCount--
-			geneticsCounts[individual.genetics]--
-			deadIndividuals = append(deadIndividuals, individual.id)
+		//check how many female and male individuals 
+		femaleToDie := make([]Individual,0)
+		maleToDie := make([]Individual,0)
 
-		} else if individual.sex == 0 && maleDeathCount > 0 && geneticsCounts[individual.genetics] > 0 {
-
-			// Check and select male individuals for death.
-			maleDeathCount --
-			geneticsCounts[individual.genetics] --
-			deadIndividuals = append(deadIndividuals, individual.id)
+		for i := range individualToDie {
+			if individualToDie[i].sex == 0 { //male 
+				maleToDie = append(maleToDie, individualToDie[i])
+			} else {
+				femaleToDie = append(femaleToDie, individualToDie[i])
+			}
 		}
-	}
-
-	// Remove dead individuals from the population
-	var updatedIndividuals []Individual
-	for _, individual := range population.individuals {
-		if !contains(deadIndividuals, individual.id) {
-			updatedIndividuals = append(updatedIndividuals, individual)
+		femaleReal := len(femaleToDie)
+		maleReal := len(maleToDie)
+		*/
+		//newIndividual := CopyIndividual(population.individuals)
+		if count < totalDeathCount {
+			totalDeathCount = count 
 		}
+		for i := 0; i < totalDeathCount; i++ {
+			n := len(individualToDie)
+			randVal := rand.Intn(n)
+			indi := individualToDie[randVal]
+			population.individuals = Delete(population.individuals, indi)
+			individualToDie = Delete(individualToDie, indi)
+		}
+		//population.individuals = newIndividual
 	}
-	population.individuals = updatedIndividuals
+	
+}
 
+func CopyIndividual(individuals []Individual) []Individual {
+	newIndividual := make([]Individual, 0)
+	for _, indi := range individuals {
+		newIndividual = append(newIndividual, indi)
+	}
+	return newIndividual
 }
 
 //contains takes a slice and an element as input as returns a boolean 
@@ -87,8 +85,8 @@ func contains(slice []int, element int) bool {
 
 //UpdateAge takes a population as input and update all individuals' age of this population 
 func (pop *Population)UpdateAge() {
-	for _, inid := range pop.individuals {
-		inid.age = inid.age + 1
+	for i := range pop.individuals {
+		pop.individuals[i].age += 1
 	}
 
 }
